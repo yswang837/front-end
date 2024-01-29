a8dacbf445e6e939aa78a96ab68f8a3a7312a52f
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-01-29 12:58:58 +0800

    添加vue3代码,笔记及资源等

--- vue3-study/backup/002_一个简单的效果/App.vue
+++ vue3-study/backup/002_一个简单的效果/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="app">
     <h1>你好啊！</h1>
+    <Person/>
   </div>
 </template>
 
 <script lang="ts">
+  import Person from './components/Person.vue'
+
   export default {
-    name:'App' //组件名
+    name:'App', //组件名
+    components:{Person} //注册组件
   }
 </script>
 
