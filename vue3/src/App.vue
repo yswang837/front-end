a03c4930a23c9410f9988dc145675999e146fc9a
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-02-01 11:30:23 +0800

    写一个App组件

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="hello">
-      <h4>你好啊！</h4>
+  <div class="app">
+    <h1>你好啊！</h1>
   </div>
 </template>
 
-<script lang="ts" setup name="App">
-
+<script lang="ts">
+  export default {
+    name:'App' //组件名
+  }
 </script>
 
-<style scoped>
-  .hello{
-    background-color: gray;
-    padding: 10px;
+<style>
+  .app {
+    background-color: #ddd;
     box-shadow: 0 0 10px;
     border-radius: 10px;
+    padding: 20px;
   }
 </style>
