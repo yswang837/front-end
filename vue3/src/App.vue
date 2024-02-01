93f5fffcd8a3cb0a7cfaf12e93d8ea23c48366a9
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-02-01 11:35:58 +0800

    一个简单的效果

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="hello">
-      <h4>你好啊！</h4>
+  <div class="app">
+    <h1>你好啊！</h1>
+    <Person/>
   </div>
 </template>
 
-<script lang="ts" setup name="App">
+<script lang="ts">
+  import Person from './components/Person.vue'
 
+  export default {
+    name:'App', //组件名
+    components:{Person} //注册组件
+  }
 </script>
 
-<style scoped>
-  .hello{
-    background-color: gray;
-    padding: 10px;
+<style>
+  .app {
+    background-color: #ddd;
     box-shadow: 0 0 10px;
     border-radius: 10px;
+    padding: 20px;
   }
 </style>
