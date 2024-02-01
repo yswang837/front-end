531787d7772c97ea342d1d77d222c51d3a2b03ab
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-02-01 13:38:49 +0800

    watch监视情况1

---
+++ vue3/src/components/Person.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="person">
+    <h1>情况一：监视【ref】定义的【基本类型】数据</h1>
+    <h2>当前求和为：{{sum}}</h2>
+    <button @click="changeSum">点我sum+1</button>
+  </div>
+</template>
+
+<script lang="ts" setup name="Person">
+  import {ref,watch} from 'vue'
+  // 数据
+  let sum = ref(0)
+  // 方法
+  function changeSum(){
+    sum.value += 1
+  }
+  // 监视，情况一：监视【ref】定义的【基本类型】数据，newValue和oldValue的值是不一样的，在实际开发中，通常就用value来接收新值了，很少接收两个值
+  const stopWatch = watch(sum,(newValue,oldValue)=>{
+    console.log('sum变化了',newValue,oldValue)
+    if(newValue >= 10){
+      stopWatch()
+    }
+  })
+</script>
+
+<style scoped>
+  .person {
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+  }
+  button {
+    margin: 0 5px;
+  }
+  li {
+    font-size: 20px;
+  }
+</style>
