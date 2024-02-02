959ea91f5c2d4c9bbed0efb81467f292807b95a5
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-02-02 09:26:47 +0800

    add

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="hello">
-      <h4>你好啊！</h4>
-  </div>
+  <!-- 务必看懂下面这一行代码 -->
+  <!-- <h2 a="1+1" :b="1+1" c="x" :d="x" ref="qwe">测试</h2> -->
+  <!-- 父向子传递数据 -->
+  <Person :list="personList" /> 
+  <!-- 不是传递的Persons类型，传递的字符串类型 -->
+  <!-- <Person list="personList" />  -->
+  <!-- 什么也不传，就用默认值 -->
+  <!-- <Person/>  -->
 </template>
 
 <script lang="ts" setup name="App">
+  import Person from './components/Person.vue'
+  import {reactive} from 'vue'
+  import {type Persons} from '@/types'
+
+  let x = 9
+
+  let personList = reactive<Persons>([
+    {id:'asudfysafd01',name:'张三',age:18},
+    {id:'asudfysafd02',name:'李四',age:20},
+    {id:'asudfysaf)d03',name:'王五',age:22}
+  ])
 
 </script>
-
-<style scoped>
-  .hello{
-    background-color: gray;
-    padding: 10px;
-    box-shadow: 0 0 10px;
-    border-radius: 10px;
-  }
-</style>
