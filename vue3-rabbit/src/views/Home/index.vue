8ddecd9add1dff2602d93fc9d0cdbf40fed9f813
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-02-03 07:13:29 +0800

    home整体结构拆分以及分类数据渲染

--- vue3-rabbit/src/views/Home/index.vue
+++ vue3-rabbit/src/views/Home/index.vue
@@ -1,13 +1,17 @@
-<template>
-    <div class="" style="height: 1200px;">
-        我是home页
-    </div>
-</template>
-
-<script lang="ts" setup name="">
-
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import homeProduct from './components/HomeProduct.vue'
 </script>
 
-<style scoped>
-
-</style>
+<template>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <homeProduct />
+</template>
