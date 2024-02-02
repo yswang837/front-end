f659fbf9a2571cf9cd960ed13f237c07d6666a47
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-02-02 13:45:00 +0800

    嵌套路由

---
+++ vue3/src/router/index.ts
@@ -0,0 +1,34 @@
+import {createRouter,createWebHistory} from 'vue-router'
+import Home from '@/pages/Home.vue'
+import News from '@/pages/News.vue'
+import About from '@/pages/About.vue'
+import Detail from '@/pages/Detail.vue'
+
+const router = createRouter({
+  history:createWebHistory(),
+  routes:[
+    {
+      name:'zhuye',
+      path:'/home',
+      component:Home
+    },
+    {
+      name:'xinwen',
+      path:'/news',
+      component:News,
+      children:[
+        {
+          path:'detail',
+          component:Detail
+        }
+      ]
+    },
+    {
+      name:'guanyu',
+      path:'/about',
+      component:About
+    },
+  ]
+})
+
+export default router
