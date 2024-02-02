013d3b82f075f01c5a2e045aa392493bddf1def3
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-02-02 13:11:50 +0800

    路由基本切换效果

---
+++ vue3/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img src="http://www.atguigu.com/images/index_new/logo.png" alt="尚硅谷logo">
+  </div>
+</template>
+
+<script setup lang="ts" name="Home">
+
+</script>
+
+<style scoped>
+  .home {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+</style>
