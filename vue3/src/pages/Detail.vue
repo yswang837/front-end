956f11ea4b2b0bb7a815011c03267c75a48b97f0
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-02-02 17:08:55 +0800

    路由的query参数

---
+++ vue3/src/pages/Detail.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul class="news-list">
+    <li>编号：{{ query.id }}</li>
+    <li>标题：{{ query.title }}</li>
+    <li>内容：{{ query.content }}</li>
+  </ul>
+</template>
+
+<script setup lang="ts" name="About">
+  import {toRefs} from 'vue'
+  import {useRoute} from 'vue-router'
+  let route = useRoute() //拿到路由器的路由
+  let {query} = toRefs(route) // 获取路由的query参数，模板就可以渲染了
+
+
+</script>
+
+<style scoped>
+  .news-list {
+    list-style: none;
+    padding-left: 20px;
+  }
+
+  .news-list>li {
+    line-height: 30px;
+  }
+</style>
---
+++ vue3/src/router/index.ts
@@ -0,0 +1,36 @@
+import {createRouter,createWebHistory} from 'vue-router'
+// 引入一个一个可能要呈现组件
+import Home from '@/pages/Home.vue'
+import News from '@/pages/News.vue'
+import About from '@/pages/About.vue'
+import Detail from '@/pages/Detail.vue'
+
+const router = createRouter({
+  history:createWebHistory(),
+  routes:[ // 给每个路由组件都加上了name属性，可通过name跳转过来
+    {
+      name:'zhuye',
+      path:'/home',
+      component:Home
+    },
+    {
+      name:'xinwen',
+      path:'/news',
+      component:News,
+      children:[
+        {
+          name:'xiangqing',
+          path:'detail',
+          component:Detail
+        }
+      ]
+    },
+    {
+      name:'guanyu',
+      path:'/about',
+      component:About
+    }
+  ]
+})
+
+export default router
