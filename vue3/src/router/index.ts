b333697baad7006cca252c919de332f1be220a84
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-02-02 13:22:43 +0800

    路由组件的两个注意点

---
+++ vue3/src/router/index.ts
@@ -0,0 +1,33 @@
+// 创建一个路由器，并暴露出去
+
+// 路由组件一般放在pages文件夹views文件夹，一般组件放在components文件夹
+// 路由组件通过路由规则来调用，一般组件通过程序员写出来调用，如本例的<Header/>
+
+// 第一步：引入createRouter
+import {createRouter,createWebHistory} from 'vue-router'
+// 引入一个一个可能要呈现组件
+import Home from '@/pages/Home.vue'
+import News from '@/pages/News.vue'
+import About from '@/pages/About.vue'
+
+// 第二步：创建路由器
+const router = createRouter({
+  history:createWebHistory(), //路由器的工作模式（稍后讲解）
+  routes:[ //一个一个的路由规则
+    {
+      path:'/home',
+      component:Home
+    },
+    {
+      path:'/news',
+      component:News
+    },
+    {
+      path:'/about',
+      component:About
+    },
+  ]
+})
+
+// 暴露出去router
+export default router
