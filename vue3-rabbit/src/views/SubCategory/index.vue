456f556d96dc948337c56775578b32c1678441db
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-02-05 13:53:28 +0800

    二级分类-列表无限加载

--- vue3-rabbit/src/views/SubCategory/index.vue
+++ vue3-rabbit/src/views/SubCategory/index.vue
@@ -34,6 +34,20 @@ const tabChange = ()=>{
     getGoodList()
 }
 
+const disabled = ref(false)
+
+// 加载更多
+const load = async ()=>{
+    // console.log('加载更多数据....');
+    reqData.value.page++
+    const res = await getSubCategoryAPI(reqData.value)
+    goodList.value = [...goodList.value,...res.result.items]
+    // 加载完毕，停止监听
+    if(res.result.items.length === 0) {
+        disabled.value = true
+    }
+}
+
 </script>
 
 <template>
@@ -53,7 +67,7 @@ const tabChange = ()=>{
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
          <!-- 商品列表-->
         <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id"/>
       </div>
