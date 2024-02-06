b69881e0214d0a605b5d455fac2b602c62615fe8
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-02-07 02:36:41 +0800

    通用组件统一全局注册

---
+++ vue3-rabbit/src/components/index.js
@@ -0,0 +1,10 @@
+// 把components下的所有组件都进行全局化注册
+// 通过插件的方式
+import ImageView from './ImageView/index.vue'
+import Sku from './XtxSku/index.vue'
+export const componentsPlugin = {
+    install(app) {
+        app.component('XtxImageView',ImageView)
+        app.component('XtxSku',Sku)
+    }
+}
--- vue3-rabbit/src/main.js
+++ vue3-rabbit/src/main.js
@@ -7,11 +7,14 @@ import router from './router'
 import '../styles/common.scss'
 
 import {lazyPlugin} from '../directives/'
+// 引入全局组件插件
+import {componentsPlugin} from './components/index'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(lazyPlugin)
+app.use(componentsPlugin)
 
 app.mount('#app')
