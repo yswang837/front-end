f66ba79d2f58b767d15438d097b42cbaba268f29
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-02-22 12:59:24 +0800

    登录-表单校验-统一校验

--- vue3-rabbit/src/views/Login/index.vue
+++ vue3-rabbit/src/views/Login/index.vue
@@ -31,6 +31,19 @@ const rules = {
     ]
 }
 
+//3、获取form实例做统一校验
+const formRef = ref(null)
+const doLogin =()=>{
+  // 调用实例方法
+  formRef.value.validate((valid)=>{
+    // valid: 所有表单都通过校验，它才为true
+    console.log(valid);
+    if(valid){
+      // todo 登录逻辑
+    }
+  })
+}
+
 </script>
 
 
@@ -55,7 +68,7 @@ const rules = {
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
+            <el-form ref="formRef" :model="form" :rules="rules" label-position="right" label-width="60px"
               status-icon>
               <el-form-item prop="account" label="账户">
                 <el-input v-model="form.account"/>
@@ -68,7 +81,7 @@ const rules = {
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="doLogin">点击登录</el-button>
             </el-form>
           </div>
         </div>
