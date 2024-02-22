dd0196dea586923d07c54d362318b20c8074cd38
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-02-22 13:54:00 +0800

    登录-登录与非登录状态渲染不同模板

--- vue3-rabbit/src/views/Layout/components/LayoutNav.vue
+++ vue3-rabbit/src/views/Layout/components/LayoutNav.vue
@@ -1,5 +1,6 @@
 <script setup>
-
+import { useUserStore } from '../../../stores/user';
+const userStore = useUserStore()
 </script>
 
 <template>
@@ -7,8 +8,9 @@
     <div class="container">
       <ul>
         <!-- 多模板渲染，用于区分登录状态和非登录状态 -->
-        <template v-if="false">
-          <li><a href="javascript:;"><i class="iconfont icon-user"></i>周杰伦</a></li>
+        <!-- 适配思路：登录时显示第一块，非登录时显示第二块，是否有token -->
+        <template v-if="userStore.userInfo.token">
+          <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{ userStore.userInfo.account }}</a></li>
           <li>
             <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
               <template #reference>
