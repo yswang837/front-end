e692f194f51b20f35f145618d7f2dcb58f3c9f63
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-02-23 12:52:27 +0800

    登录-请求拦截器携带token

--- vue3-rabbit/src/utils/http.js
+++ vue3-rabbit/src/utils/http.js
@@ -3,14 +3,24 @@
 import axios from "axios";
 import 'element-plus/theme-chalk/el-message.css'
 import { ElMessage } from 'element-plus'
+import {useUserStore} from '@/stores/user'
 
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
     timeout: 5000
 })
 
+
+
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
+    // 1、从pinia从获取token数据
+    const userStore = useUserStore()
+    const token = userStore.userInfo.token
+    // 2、按照后端要拼接token数据
+    if(token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
     return config
   }, e => Promise.reject(e))
   
