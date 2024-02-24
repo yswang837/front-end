8db1fc74784a3429ece1e1878f5d4b89a37a5df0
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-02-24 20:38:02 +0800

    element plus主题定制

--- rabbit/vite.config.js
+++ rabbit/vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import ElementPlus from 'unplugin-element-plus/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,7 +20,10 @@ export default defineConfig({
         ElementPlusResolver({importStyle: "sass"})
       ]
     }),
-
+    // 按需定制主题配置
+    ElementPlus({
+      useSource: true,
+    }),
   ],
   resolve: {
     alias: {
