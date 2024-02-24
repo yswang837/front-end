9afbfd98a807307d1c79281f8dec7674882fa3ed
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-02-24 21:50:17 +0800

    scss文件的自动导入

--- rabbit/src/App.vue
+++ rabbit/src/App.vue
@@ -3,11 +3,19 @@
 </script>
 
 <template>
+  
   <!-- 一级路由出口组件 -->
   <RouterView />
+  <div class="test">
+    test scss
+  </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.test{
+  color: $priceColor;
+}
+
 header {
   line-height: 1.5;
   max-height: 100vh;
