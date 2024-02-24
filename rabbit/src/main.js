ee724f9adff7c6633cde4da7aa3c9ba5e5abe26d
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-02-24 21:39:04 +0800

    静态资源的引入

--- rabbit/src/main.js
+++ rabbit/src/main.js
@@ -1,10 +1,9 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+// 引入初始化的样式文件
+import '@/styles/common.scss'
 import { getCategoryAPI } from '@/apis/testAPI'
 const res = getCategoryAPI()
 console.log('AA',res);
