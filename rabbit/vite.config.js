495adca5ec5d94d7a0e2069450fa28b9d5718603
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-02-24 20:28:11 +0800

    引入element plus库

--- rabbit/vite.config.js
+++ rabbit/vite.config.js
@@ -1,15 +1,25 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    // 配置插件
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    })
   ],
   resolve: {
     alias: {
+      // 真正解析@路径的地方，jsconfig.json配的只是联想提示
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
