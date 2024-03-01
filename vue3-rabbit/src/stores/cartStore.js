6b7164663b487e26db36952702a569173182816e
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-03-01 13:05:15 +0800

    本地-列表购物车单选功能

--- vue3-rabbit/src/stores/cartStore.js
+++ vue3-rabbit/src/stores/cartStore.js
@@ -27,13 +27,19 @@ export const useCartStore = defineStore('cart', () => {
         const idx = cartList.value.findIndex((item)=>skuId === item.skuId)
         cartList.value.splice(idx,1)
     }
+    // 单选功能
+    const singleCheck = (skuId,selected)=>{
+        // 通过skuId找到要修改的那项，修改为selected
+        const item = cartList.value.find((item)=>skuId === item.skuId)
+        item.selected = selected
+    }
     // 计算属性
     // 1.总的数量：所有项的count之和
     // 2.总价：所有项的count*price
     const allCount = computed(()=> cartList.value.reduce((a,c)=>a+c.count, 0))
     const allPrice = computed(()=> cartList.value.reduce((a,c)=>a+c.count*c.price, 0))
 
-    return {cartList, allCount, allPrice, addCart, delCart}
+    return {cartList, allCount, allPrice, addCart, delCart, singleCheck}
 },{
     persist: true
 })
