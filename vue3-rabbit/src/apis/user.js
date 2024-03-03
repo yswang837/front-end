42eacac381368140189fefaf673f912622669f95
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-03-03 15:47:13 +0800

    会员中心-个人信息数据渲染

--- vue3-rabbit/src/apis/user.js
+++ vue3-rabbit/src/apis/user.js
@@ -10,3 +10,13 @@ export const loginAPI = ({account,password})=>{
         }
     })
 }
+
+// 猜你喜欢接口
+export const getLikeListAPI = ({ limit = 4 }) => {
+    return httpInstance({
+      url:'/goods/relevant',
+      params: {
+        limit 
+      }
+    })
+  }
--- vue3-rabbit/src/router/index.js
+++ vue3-rabbit/src/router/index.js
@@ -10,8 +10,8 @@ import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
 import Member from '@/views/Member/index.vue'
-import UserInfo from '@/views/Member/components/UserInfo.vue'
-import UserOrder from '@/views/Member/components/UserOrder.vue'
+import UserInfo from '@/views/Member/Components/UserInfo.vue'
+import UserOrder from '@/views/Member/Components/UserOrder.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
