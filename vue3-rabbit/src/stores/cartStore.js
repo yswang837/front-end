9f76910be66ef27bd7d88c83431fe88697aad162
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-03-03 08:52:08 +0800

    退出登录-清空购物车信息

--- vue3-rabbit/src/stores/cartStore.js
+++ vue3-rabbit/src/stores/cartStore.js
@@ -56,6 +56,11 @@ export const useCartStore = defineStore('cart', () => {
         } 
     }
 
+    // 清除购物车
+    const clearCart = ()=>{
+        cartList.value = []
+    }
+
     
     // 单选功能
     const singleCheck = (skuId,selected)=>{
@@ -80,7 +85,11 @@ export const useCartStore = defineStore('cart', () => {
     // 是否全选
     const isAll = computed(()=> cartList.value.every((item) => item.selected))
 
-    return {cartList, allCount, allPrice, selectedCount, selectedPrice, isAll, addCart, delCart, singleCheck, allCheck}
+    return {
+        cartList, 
+        allCount, allPrice, selectedCount, selectedPrice, isAll, 
+        addCart, delCart, singleCheck, allCheck, clearCart,
+    }
 },{
     persist: true
 })
--- vue3-rabbit/src/stores/userStore.js
+++ vue3-rabbit/src/stores/userStore.js
@@ -3,9 +3,11 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import { loginAPI } from '@/apis/user'
+import { useCartStore } from './cartStore'
 // import { loginAPI } from '@/'
 
 export const useUserStore = defineStore('user', () => {
+  const carStore = useCartStore()
   // 1、数据
   const userInfo = ref({})
   // 2、方法
@@ -17,6 +19,8 @@ export const useUserStore = defineStore('user', () => {
   // 退出时清除用户信息
   const clearUserInfo =()=>{
     userInfo.value = {}
+    // 执行清除购物车的action函数
+    carStore.clearCart()
   }
   
   // 3、返回
