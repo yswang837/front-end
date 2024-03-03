a0562df2a0b8b70ef4df74e4944861f58a781617
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-03-03 10:52:37 +0800

    结算-生成订单功能

--- vue3-rabbit/src/views/Checkout/index.vue
+++ vue3-rabbit/src/views/Checkout/index.vue
@@ -1,9 +1,12 @@
 <script setup>
-import { getCheckInfoAPI } from '@/apis/checkout'
+import { getCheckInfoAPI,createOrderAPI } from '@/apis/checkout'
 import { onMounted, ref } from 'vue';
+import {useRouter} from 'vue-router'
+import { useCartStore } from '@/stores/cartStore'
 const checkInfo = ref({})  // 订单对象
 const curAddress = ref({}) // 默认地址
-
+const router = useRouter()
+const cartStore = useCartStore()
 const getCheckInfo = async ()=> {
     const res = await getCheckInfoAPI()
     checkInfo.value = res.result
@@ -30,6 +33,32 @@ const confirm = ()=>{
 
 const cancel = ()=> {
   showDialog.value = false
+}
+
+// 创建订单
+const createOrder = async ()=>{
+  const res = await createOrderAPI({
+    deliveryTimeType:1,
+    payType:1,
+    payChannel:1,
+    buyerMessage:'',
+    goods: checkInfo.value.goods.map(item=>{
+      return {
+        skuId: item.skuId,
+        count: item.count
+      }
+    }),
+    addressId: curAddress.value.id
+  })
+  const orderId = res.result.id
+  router.push({
+    path:'/pay',
+    query:{
+      id:orderId
+    }
+  })
+  // 更新购物车
+  cartStore.updateNewList()
 }
 
 </script>
@@ -126,7 +155,7 @@ const cancel = ()=> {
         </div>
         <!-- 提交订单 -->
         <div class="submit">
-          <el-button type="primary" size="large" >提交订单</el-button>
+          <el-button type="primary" @click="createOrder" size="large" >提交订单</el-button>
         </div>
       </div>
     </div>
