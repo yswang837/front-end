3bd0288eb7790b7aaaff56e4854ca924ca7ab12b
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-03-03 10:16:55 +0800

    结算-切换地址的激活和交互

--- vue3-rabbit/src/views/Checkout/index.vue
+++ vue3-rabbit/src/views/Checkout/index.vue
@@ -2,7 +2,7 @@
 import { getCheckInfoAPI } from '@/apis/checkout'
 import { onMounted, ref } from 'vue';
 const checkInfo = ref({})  // 订单对象
-const curAddress = ref({})
+const curAddress = ref({}) // 默认地址
 
 const getCheckInfo = async ()=> {
     const res = await getCheckInfoAPI()
@@ -15,6 +15,22 @@ onMounted(()=>getCheckInfo())
 
 // 控制弹窗打开
 const showDialog = ref(false)
+
+// 切换地址
+const activeAddress = ref({})
+const switchAddress = (item)=>{
+  activeAddress.value = item
+}
+
+const confirm = ()=>{
+  curAddress.value = activeAddress.value
+  showDialog.value = false
+  activeAddress.value = {}
+}
+
+const cancel = ()=> {
+  showDialog.value = false
+}
 
 </script>
 
@@ -118,7 +134,7 @@ const showDialog = ref(false)
   <!-- 切换地址 -->
   <el-dialog v-model="showDialog" title="切换收货地址" width="30%" center>
   <div class="addressWrapper">
-    <div class="text item" v-for="item in checkInfo.userAddresses"  :key="item.id">
+    <div class="text item" :class="{active:activeAddress.id === item.id}" @click="switchAddress(item)" v-for="item in checkInfo.userAddresses"  :key="item.id">
       <ul>
       <li><span>收<i />货<i />人：</span>{{ item.receiver }} </li>
       <li><span>联系方式：</span>{{ item.contact }}</li>
@@ -128,8 +144,8 @@ const showDialog = ref(false)
   </div>
   <template #footer>
     <span class="dialog-footer">
-      <el-button>取消</el-button>
-      <el-button type="primary">确定</el-button>
+      <el-button @click="cancel">取消</el-button>
+      <el-button type="primary" @click="confirm">确定</el-button>
     </span>
   </template>
 </el-dialog>
