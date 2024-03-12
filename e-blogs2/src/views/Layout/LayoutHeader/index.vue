de8f54b14e80eda8092e9ff995309678d21e2527
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-03-12 19:08:48 +0800

    header导航栏

--- e-blogs2/src/views/Layout/LayoutHeader/index.vue
+++ e-blogs2/src/views/Layout/LayoutHeader/index.vue
@@ -4,10 +4,41 @@
 
 <template>
   <div class="container">
-    <div class="header">头部导航</div>
+    <ul>
+      <template v-if="true">
+        <li><a href="javascript:;">周杰伦</a></li>
+              <li>
+                <el-popconfirm @confirm="confirm" title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
+                  <template #reference>
+                    <a href="javascript:;">退出登录</a>
+                  </template>
+                </el-popconfirm>
+              </li>
+              <li><a href="javascript:;">我的订单</a></li>
+              <li><a href="javascript:;">会员中心</a></li>
+      </template>
+      <template v-else>
+          <li><a href="javascript:;">请先登录</a></li>
+          <li><a href="javascript:;">帮助中心</a></li>
+          <li><a href="javascript:;">关于我们</a></li>
+      </template>
+    </ul>
   </div>
 </template>
 
 <style scoped>
-
+.container ul {
+  display: flex;
+  background: #333;
+  height:50px;
+  justify-content: flex-end;
+  align-items: center;
+  li {
+    a {
+      color: #fff;
+      padding: 0 15px;
+      line-height: 1;
+    }
+  }
+}
 </style>
