536af27f83e3ec5f523198897526074bcc2db588
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-03-13 14:07:29 +0800

    登录组件

---
+++ e-blogs2/src/views/Layout/LayoutHeader/Login/index.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+
+
+let x = defineProps(['showLoginDialog'])
+const showDialog = ref(x.showLoginDialog)
+console.log(showDialog.value);
+</script>
+
+<template>
+  <el-dialog v-model="showDialog" title="切换收货地址" width="30%" center>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="showLoginDialog">取消</el-button>
+        <el-button type="primary" @click="showLoginDialog">确定</el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<style scoped>
+
+</style>
--- e-blogs2/src/views/Layout/LayoutHeader/index.vue
+++ e-blogs2/src/views/Layout/LayoutHeader/index.vue
@@ -1,11 +1,17 @@
 <script setup>
-
+import Login from './Login/index.vue'
+import { ref } from 'vue'
+const isClickLogin = ref(false)
+const toLoginComponent = ()=>{
+  isClickLogin.value = true
+  console.log(isClickLogin.value);
+}
 </script>
 
 <template>
   <div class="container">
     <ul>
-      <template v-if="true">
+      <template v-if="false">
         <li><a href="javascript:;">周杰伦</a></li>
               <li>
                 <el-popconfirm @confirm="confirm" title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
@@ -18,7 +24,7 @@
               <li><a href="javascript:;">会员中心</a></li>
       </template>
       <template v-else>
-          <li><a href="javascript:;">请先登录</a></li>
+          <li><a href="javascript:;" @click="toLoginComponent()">请先登录</a><Login :showLoginDialog="`isClickLogin`" /></li>
           <li><a href="javascript:;">帮助中心</a></li>
           <li><a href="javascript:;">关于我们</a></li>
       </template>
