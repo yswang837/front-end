90dab45dea896a8a0cd4ff29f3de3b132c4c7bf7
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-03-13 19:21:21 +0800

    登录框中的账密登录

--- e-blogs2/src/views/Layout/LayoutHeader/Login/index.vue
+++ e-blogs2/src/views/Layout/LayoutHeader/Login/index.vue
@@ -3,13 +3,28 @@ import { ref } from 'vue';
 // 控制登录的弹窗是否显示
 const showLoginDialog = ref(false)
 defineExpose({showLoginDialog})
+
+// 默认显示账密登录的tab
+const activeName = ref('first')
+
+// 账密登录输入框
+const username = ref('')
+const password = ref('')
+
 </script>
 
 <template>
-  <el-dialog v-model="showLoginDialog" title="登录" width="30%" center>
+  <el-dialog v-model="showLoginDialog" width="30%" center :show-close="false">
+    <el-tabs v-model="activeName" class="demo-tabs" :stretch="true" @tab-click="handleClick">
+      <el-tab-pane label="账密登录" name="first">
+        <el-input v-model="username" style="width: 240px" placeholder="Type something" :prefix-icon="Search" />
+        <el-input v-model="password" style="width: 240px" placeholder="Type something" :prefix-icon="Search" type="password"/>
+      </el-tab-pane>
+      <el-tab-pane label="验证码登录" name="second">手机验证码或者邮箱验证码登录</el-tab-pane>
+      <el-tab-pane label="微信扫码登录" name="third">微信扫码登录</el-tab-pane>
+    </el-tabs>
     <template #footer>
       <span class="dialog-footer">
-        <el-button @click="showLoginDialog=false">取消</el-button>
         <el-button type="primary" @click="showLoginDialog=false">确定</el-button>
       </span>
     </template>
