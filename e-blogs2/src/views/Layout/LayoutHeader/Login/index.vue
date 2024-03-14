88bf9e6088c17e6c4547528c3ed573327fa0c67b
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-03-14 14:03:16 +0800

    验证码登录的结构基本完成

--- e-blogs2/src/views/Layout/LayoutHeader/Login/index.vue
+++ e-blogs2/src/views/Layout/LayoutHeader/Login/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
 // 控制登录的弹窗是否显示
 const showLoginDialog = ref(false)
@@ -14,15 +14,38 @@ const password = ref('')
 const identifyCode1 = ref('')
 
 // 邮箱登录输入框
-const identifyCode2 = ref('')
-const email = ref('')
+const form1 = reactive({
+  email:'',
+  identifyCode1:'',
+  agree:true
+})
 
 </script>
 
 <template>
   <el-dialog v-model="showLoginDialog" width="30%" center :show-close="false">
-    <el-tabs v-model="activeName" class="demo-tabs" :stretch="true" @tab-click="handleClick">
-      <el-tab-pane label="账密登录" name="first">
+    <el-tabs v-model="activeName" class="demo-tabs" :stretch="true">
+      <el-tab-pane label="验证码登录" name="first">
+        <el-form :model="form1" label-width="60px" status-icon>
+              <el-form-item prop="email" label="邮&nbsp;&nbsp;&nbsp;箱">
+                <el-input v-model="form1.email" placeholder="请输入邮箱" clearable/>
+              </el-form-item>
+              <el-form-item prop="identityCode1" label="验证码">
+                <el-input v-model="form1.identifyCode1" placeholder="请输入验证码" clearable />
+                <el-button>获取验证码</el-button>
+              </el-form-item>
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox  size="large" v-model="form1.agree">
+                  我已同意隐私条款和服务条款
+                </el-checkbox>
+              </el-form-item>
+              <el-button size="large" class="subBtn">登录</el-button>
+            </el-form>
+      </el-tab-pane>
+      <el-tab-pane label="微信扫码登录" name="second">
+        <img src="@/assets/images/wx-login.png" width="200px">
+      </el-tab-pane>
+      <el-tab-pane label="账密登录" name="third">
         <el-input v-model="username" style="width: 240px" placeholder="请输入用户名" />
         <el-input v-model="password" style="width: 240px" placeholder="请输入密码" type="password" />
         <div>
@@ -30,24 +53,7 @@ const email = ref('')
           <!-- 这里放验证码图片 -->
         </div>
       </el-tab-pane>
-      <el-tab-pane label="验证码登录" name="second">
-        <el-input v-model="email" style="width: 240px" placeholder="请输入邮箱" />
-        <el-input v-model="identifyCode2" style="width: 240px" placeholder="请输入验证码" />
-      </el-tab-pane>
-      <el-tab-pane label="微信扫码登录" name="third">
-        <img src="@/assets/images/wx-login.png" width="200px">
-      </el-tab-pane>
     </el-tabs>
-    <template v-slot:footer>
-      <span class="dialog-footer">
-        <template v-if="activeName==='first'">
-          <el-button type="primary" @click="showLoginDialog=false">登录</el-button>
-        </template>
-        <template v-if="activeName==='second'">
-          <el-button type="primary" @click="showLoginDialog=false">登录/注册</el-button>
-        </template>
-      </span>
-    </template>
   </el-dialog>
 </template>
 
