88211912e87eabc9e4c1070db8088f72062a4dcd
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-03-18 12:35:26 +0800

    项目初始化

---
+++ easyblog/src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '@/views/Login.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/login',
+      component: Login
+    },
+  ]
+})
+
+export default router
---
+++ easyblog/src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        登录页
+    </div>
+    <el-button type="primary">按钮</el-button>
+    
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+body {
+    background: url(../assets/images/login-bg.jpg);
+}
+</style>
